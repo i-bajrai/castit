bc5fa9fe31f7bff2930a7b80e2515dc6f2c4c396
Imran Bajerai <imran.bajerai@adorebeauty.com.au>
2026-01-31 20:45:25 +0800

    tweak

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -144,7 +144,7 @@
                                     </div>
                                 </button>
                                 <div class="flex items-center gap-3">
-                                    <span class="text-sm text-gray-500">{{ $caItemCount }} items</span>
+                                    <span class="text-sm {{ $caItemCount > 0 ? 'text-emerald-600 font-semibold' : 'text-gray-400' }}">{{ $caItemCount }} items</span>
                                     <a href="{{ route('projects.control-accounts.line-items', [$project, $account]) }}" class="text-xs text-gray-500 hover:text-gray-700 font-medium">Manage</a>
                                     @if($isEditable)
                                         <a href="#" x-on:click.stop.prevent="$dispatch('open-modal', 'create-cost-package-{{ $account->id }}')" class="inline-flex items-center text-xs text-indigo-600 hover:text-indigo-800 font-medium">
